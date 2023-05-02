155cdad0f1291dbdafddce86eae87f4e4ef26321
Franco Felicioni <fran.felicioni@gmail.com>
2023-05-02 21:10:35 +0200

    Pinia
    Implementing destructuring of counter.js in HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,16 @@
 <script setup>
 import { useCounterStore } from '@/store/counter.js';
+import { storeToRefs } from 'pinia';
 
 const useCounter = useCounterStore()
+const {increment} = useCounter
+const { count, double } = storeToRefs(useCounter)
 </script>
 
 <template>
   <main>
-    <h1>Home counter: {{ useCounter.count }}</h1>
-    <button @click="useCounter.increment" >INCREASE</button>
-    <h2>Double {{ useCounter.double }}</h2>
+    <h1>Home counter: {{ count }}</h1>
+    <button @click="increment" >INCREASE</button>
+    <h2>Double {{ double }}</h2>
   </main>
 </template>
