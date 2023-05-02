de8662bc4111065dac72ebbbe4c623cfa9ab4d6e
Franco Felicioni <fran.felicioni@gmail.com>
2023-05-02 21:04:56 +0200

    Pinia
    Store with composition API

--- src/store/counter.js
+++ src/store/counter.js
@@ -1,18 +1,41 @@
 import { defineStore } from "pinia";
+import { ref, computed } from "vue";
 
-export const useCounterStore = defineStore("counter", {
-  // State must return an object
-  state: () => ({
-    count: 0,
-  }),
-  // Methods
-  actions: {
-    increment() {
-      this.count++;
-    },
-  },
-  // Computed properties
-  getters: {
-    double: (state) => state.count * 2,
-  },
+
+//STORE WITH OPTION API
+
+// export const useCounterStore = defineStore("counter", {
+//   // State must return an object
+//   state: () => ({
+//     count: 0,
+//   }),
+//   // Methods
+//   actions: {
+//     increment() {
+//       this.count++;
+//     },
+//   },
+//   // Computed properties
+//   getters: {
+//     double: (state) => state.count * 2,
+//   },
+// });
+
+
+// STORE WITH COMPOSITION API
+
+export const useCounterStore = defineStore("counter", () => {
+  
+    const count = ref(0);
+
+    const increment = () => count.value ++;
+
+    const double =  computed (()=> count.value *2);
+
+    return {
+        count,
+        increment,
+        double,
+    }
+
 });
